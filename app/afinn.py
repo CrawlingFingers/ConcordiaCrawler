030f17ec8bbed3e9f568638aa3263180c68b878f
Connor Bode <connor.bode@gmail.com>
2014-11-21 19:56:47 -0500

    test

--- app/afinn.py
+++ app/afinn.py
@@ -18,6 +18,9 @@ sys.setdefaultencoding('utf-8')
  
 # AFINN-111 is as of June 2011 the most recent version of AFINN
 filenameAFINN = 'app/afinn.txt'
+f = open(filenameAFINN)
+print(f.read())
+f.close()
 afinn = dict(map(lambda (w, s): (w, int(s)), [ ws.strip().split('\t') for ws in open(filenameAFINN) ]))
  
 # Word splitter pattern
