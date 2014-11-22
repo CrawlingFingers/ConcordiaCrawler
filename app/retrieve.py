c4a21ae7d842bcdc176cdc42b248773df786a245
Greg Houle <houle.greg@gmail.com>
2014-11-21 20:41:38 -0500

    connor bode

--- app/retrieve.py
+++ app/retrieve.py
@@ -13,9 +13,15 @@ def main():
     search = lucene.IndexSearcher(directory)
 
     query = lucene.QueryParser("content", analyzer).parse(query())
-    maxhits = 1000
+    maxhits = 5000
     hits = search.search(query, maxhits)
 
+    print "Found %d document(s) that matched the query '%s':" % (hits.totalHits, query)
+
+    for e in hits.scoreDocs:
+        print e.score, e.doc, e.toString()
+        doc = search.doc(e.doc)
+        print doc.get("content").encode("utf-8")
 
 if __name__ == "__main__":
     main()
