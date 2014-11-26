ff1ab500fbddb7080dfc2a29d6a1badf1f3d2a12
Connor Bode <connor.bode@gmail.com>
2014-11-26 15:53:26 -0500

    question 2

--- app/questions.py
+++ app/questions.py
@@ -35,3 +35,11 @@ print("Q1. Which is the most positive Department in ENCS at Concordia?")
 most_positive_tuple = lambda current_highest, current_tuple: current_highest if current_highest[1] > current_tuple[1] else current_tuple
 positive = reduce(most_positive_tuple, [(k, v) for k, v in sentiments.iteritems()])
 print("A:  The most positive department is " + positive[0])
+print("")
+
+# question 2
+print("Q2. Is Computer Science and Software Engineering more positive or less positive than Electrical and Computer Engineering?")
+if sentiments['computer-science-software-engineering'] > sentiments['electrical-computer']:
+  print("A:  Computer Science and Software Engineering is more positive than Electrical and Computer Engineering")
+else:
+  print("A:  Computer Science and Software Engineering is less positive than Electrical and Computer Engineering")
